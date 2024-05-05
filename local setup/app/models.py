5c4330e0e7b2e3c5253d72315e682930684053e7
sarahp16 <166588123+sarahp16@users.noreply.github.com>
2024-05-05 16:26:41 +0800

    Update models.py

--- local setup/app/models.py
+++ local setup/app/models.py
@@ -3,7 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
 class UsersInfo(db.Model):
-    __tablename__ = ''
+    __tablename__ = 'UsersInfo'
     id = db.Column(db.Integer, primary_key = True)
     name = db.Column(db.String(100), nullable = False)
     surname = db.Column(db.String(100), nullable = False)
