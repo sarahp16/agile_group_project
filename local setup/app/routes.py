a11d9075c801fde18f7a52468b1cf3f950c5a6e4
sarahp16 <166588123+sarahp16@users.noreply.github.com>
2024-05-05 17:15:03 +0800

    Update routes.py

--- local setup/app/routes.py
+++ local setup/app/routes.py
@@ -1,13 +1,14 @@
-from flask import Flask, render_template, url_for
-from app import app 
-
+from flask import Flask, render_template, url_for, request, redirect, session, flash
+from app import app, db
+from app.forms import RegistrationForm
+from app.models import Users
 
 @app.route('/')
 @app.route('/homepage')
 def homepage():
     return render_template('homepage.html')
 
-@app.route('/find_game')
+@app.route('/play/find_game')
 def find_game():
     return render_template('find_game.html')
 
@@ -22,3 +23,22 @@ def leaderboard():
 @app.route('/create')
 def create():
     return render_template('create.html')
+
+@app.route('/signup', methods = ['GET', 'POST'])
+def signup():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        name = form.name.data
+        surname = form.surname.data
+        email = form.email.data
+        password = form.password.data
+        city = form.city.data
+        suburb = form.suburb.data
+
+        new_user = Users(name = name, surname = surname, email = email, password = password, city = city, suburb = suburb)
+
+        db.session.add(new_user)
+        db.session.commit()
+        flash('Congratulations, you are now registered!')
+        return redirect(url_for('login'))
+    return render_template('registration.html', title = 'Register', form = form)
