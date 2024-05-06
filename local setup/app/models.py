e3ce6dc74453d645dcea554b306f90899a32c9f6
sarahp16 <166588123+sarahp16@users.noreply.github.com>
2024-05-06 12:26:50 +0800

    Update models.py

--- local setup/app/models.py
+++ local setup/app/models.py
@@ -4,10 +4,9 @@ db = SQLAlchemy()
 
 class UsersInfo(db.Model):
     __tablename__ = 'UsersInfo'
-    id = db.Column(db.Integer, primary_key = True)
     name = db.Column(db.String(100), nullable = False)
     surname = db.Column(db.String(100), nullable = False)
-    email = db.Column(db.String(90), unique = True, nullable = False)
+    email = db.Column(db.String(90), unique = True, primary_key = True, nullable = False)
     password = db.Column(db.String(50), nullable = False)
     city = db.Column(db.String(50), nullable = False)
     suburb = db.Column(db.String(50), nullable = False)
