f86921008b929867b17dd45aba9e0c629127d35a
sarahp16 <166588123+sarahp16@users.noreply.github.com>
2024-05-08 15:35:51 +0800

    Update forms.py

--- local setup/app/forms.py
+++ local setup/app/forms.py
@@ -2,12 +2,20 @@ from flask_wtf import FlaskForm
 from wtforms import IntegerField, StringField, SubmitField, BooleanField, PasswordField
 from wtforms.validators import DataRequired, Email, EqualTo
 
+from app import db
+from app.models import UsersInfo
+
 class RegistrationForm(FlaskForm):
     name = StringField('First Name', validators=[DataRequired()])
     surname = StringField('Last Name', validators=[DataRequired()])
-    email = StringField('Email', validators=[DataRequired(), Email()])
+    email = StringField('Email Address', validators=[DataRequired()])
     password = PasswordField('Password', validators=[DataRequired()])
     confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
     city = StringField('City', validators=[DataRequired()])
     suburb = StringField('Suburb', validators=[DataRequired()])
     submit = SubmitField('Sign Up')
+
+class LoginForm(FlaskForm):
+    email = StringField('Email', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    submit = SubmitField('Sign In')
