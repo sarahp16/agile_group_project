63fb7038a7d1bba74cd932b0c9d45674879f3e2b
cimi03 <165989079+cimi03@users.noreply.github.com>
2024-05-10 20:24:13 +0800

    Update models.py
    Added player tracker

--- local setup/app/models.py
+++ local setup/app/models.py
@@ -15,6 +15,8 @@ class UsersInfo(UserMixin, db.Model):
     city: so.Mapped[str] = so.mapped_column(sa.String(64), index = True)
     suburb: so.Mapped[str] = so.mapped_column(sa.String(64), index = True)
     
+    player_tracker = so.relationship('PlayerTracker', back_populates='user')
+
     def get_password(email):
         user = UsersInfo.query.filter_by(email=email).first()
         if user:
@@ -36,20 +38,19 @@ class Quests(db.Model):
     suburb: so.Mapped[str] = so.mapped_column(sa.String(100), index = True)
     completion: so.Mapped[bool] = so.mapped_column(sa.Boolean, default = False)
 
-    hints: so.WriteOnlyMapped['Hints'] = so.relationship(back_populates='quest')
-    solutions: so.WriteOnlyMapped['Solutions'] = so.relationship(back_populates='quest')
+    hintsolution: so.WriteOnlyMapped['HintsSolutions'] = so.relationship(back_populates='quest')
 
-class Hints(db.Model):
+class HintsSolutions(db.Model):
     id: so.Mapped[int] = so.mapped_column(primary_key = True, autoincrement=True)
     hint_text: so.Mapped[str] = so.mapped_column(sa.String(300), index = True)
-    quest_id: so.Mapped[int] = so.mapped_column(sa.ForeignKey(Quests.id), index = True)
-
-    quest: so.Mapped[Quests] = so.relationship(back_populates="hints")
-
-class Solutions(db.Model):
-    id: so.Mapped[int] = so.mapped_column(primary_key = True, autoincrement=True)
     solution_text: so.Mapped[str] = so.mapped_column(sa.String(150), index = True)
     quest_id: so.Mapped[int] = so.mapped_column(sa.ForeignKey(Quests.id), index = True)
-    hint_id: so.Mapped[int] = so.mapped_column(sa.ForeignKey(Hints.id), index = True)
 
-    quest: so.Mapped[Quests] = so.relationship(back_populates="solutions")
+    quest: so.Mapped[Quests] = so.relationship(back_populates="hintsolution")
+
+class PlayerTracker(db.Model):
+    user_id: so.Mapped[int] = so.mapped_column(sa.ForeignKey(UsersInfo.id), primary_key = True, index = True)
+    points: so.Mapped[int] = so.mapped_column(index = True, default = 0)
+    quests_completed: so.Mapped[int] = so.mapped_column(index = True, default = 0)
+
+    user = so.relationship('UsersInfo', back_populates='player_tracker')
