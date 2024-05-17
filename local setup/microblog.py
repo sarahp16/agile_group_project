ab335de7488f0273ee8e945796d63c6ec58ca048
sarahp16 <166588123+sarahp16@users.noreply.github.com>
2024-05-18 00:01:04 +0800

    Update microblog.py

--- local setup/microblog.py
+++ local setup/microblog.py
@@ -3,6 +3,8 @@ import sqlalchemy.orm as so
 from app import app, db
 from app.models import UsersInfo
 
+#Running app
+
 @app.shell_context_processor
 def make_shell_context():
    return {'sa':sa, 'so':so, 'db':db, 'UsersInfo':UsersInfo}
