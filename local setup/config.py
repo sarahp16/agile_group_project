39f546b13a09864babbfbba452a76111dbb13e90
sarahp16 <166588123+sarahp16@users.noreply.github.com>
2024-05-18 00:00:35 +0800

    Update config.py

--- local setup/config.py
+++ local setup/config.py
@@ -1,6 +1,7 @@
 import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
+# Setting up database URI
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY', 'default_secret_key')
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
